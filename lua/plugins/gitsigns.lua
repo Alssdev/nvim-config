23d2f458bf3b545b5bee8fa6b9c55c4dbf0ccadd
Alssdev <alssdev3@gmail.com>
2024-03-09 22:46:24 -0600

    init: Startup configuration from videos

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  config = function ()
+    require('gitsigns').setup({
+    })
+
+    local gs = package.loaded.gitsigns
+
+    vim.keymap.set('n', '<space>hp', gs.preview_hunk)
+    vim.keymap.set('n', '<space>hr', gs.reset_hunk)
+  end
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<space>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<space>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<space>fg', builtin.live_grep, {})
+    end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function ()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+        }
+      }
+      -- To get ui-select loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require("telescope").load_extension("ui-select")
+    end
+  }
+}
